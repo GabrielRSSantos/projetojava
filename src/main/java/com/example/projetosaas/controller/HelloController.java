569db3cc95c93b88552eb02ce2ffaa067e31f65f
GabrielRSSantos <gabrielsantos.new@gmail.com>
2025-06-22 17:17:17 -0300

    Commit Cors

--- src/main/java/com/example/projetosaas/controller/HelloController.java
+++ src/main/java/com/example/projetosaas/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.projetosaas.controller;
 
 import com.example.projetosaas.data.MessageResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
 
