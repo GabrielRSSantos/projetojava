22e1c1f91fbe07b8e2abe72bf66dc30427bdc4b8
GabrielRSSantos <gabrielsantos.new@gmail.com>
2025-06-22 17:58:54 -0300

    Add

--- src/main/java/com/example/projetosaas/config/WebConfig.java
+++ src/main/java/com/example/projetosaas/config/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // aplica a todas as rotas
-                        .allowedOrigins("http://localhost:5173") // frontend local
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // substitui allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
